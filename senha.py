23ec043236d7aec820a911f1cdc8d7157d54393a
Gabriel-Coimbra-Ferreira <coimbralene3@gmail.com>
2024-01-23 22:27:42 -0300

    Gerar senha
    gerador de senha com conferidor de senha correta

--- senha.py
+++ senha.py
@@ -1,16 +1,47 @@
-#biblioteca que permite a geração dos numeros aleatorios
 import random
-#aqui eu estou puxando a biblioteca e declarando a variavel "senha de acesso" que pegara o numero que essa biblioteca gerar
-Senha_de_acesso = random.randint(0, 9999)
-#aqui estou declarando que pelo menos tenham 3 zeros a esquerda ou ao menos 4 numeros
-senha_zero_esq = f'{Senha_de_acesso:04}'
 
-print("Sua senha é")
-print(senha_zero_esq)
-senha_correta = input("Qual é a sua senha?")  # Input como string
 
-# Converte a senha_correta para inteiro antes da comparação
-if int(senha_zero_esq) == int(senha_correta):
-    print("Senha correta, parabéns!")
-else:
-    print("Senha incorreta, tente novamente.")
+def gerar_senha_complexa():
+    caracteres = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_-+=[]{}|;:'<>,.?/"
+    senha_complexa = ''.join(random.sample(caracteres, 12))
+    return senha_complexa
+
+
+tentativas_maximas = 3
+tentativas = 0
+
+senha_zero_esq = f'{random.randint(0, 9999):04}'
+senha_complexa = gerar_senha_complexa()
+
+while tentativas < tentativas_maximas:
+    print("Escolha uma opção:")
+    print("1. Gerar senha de acesso")
+    print("2. Tentar entrar")
+
+    opcao = input("Digite o número da opção: ")
+
+    if opcao == "1":
+        senha_zero_esq = f'{random.randint(0, 9999):04}'
+        print("Sua nova senha de acesso é:")
+        print(senha_zero_esq)
+    elif opcao == "2":
+        senha_correta = input("Qual é a sua senha?")  # Input como string
+
+        if senha_zero_esq == senha_correta:
+            print("Senha de acesso correta, parabéns!")
+            break
+        elif senha_correta == senha_complexa:
+            print("Senha complexa correta, parabéns!")
+            break
+        else:
+            if tentativas < tentativas_maximas - 1:
+                print("Senha incorreta. Tente novamente.")
+            else:
+                print("Senha incorreta. Última tentativa.")
+
+            tentativas += 1
+    else:
+        print("Opção inválida. Tente novamente.")
+
+if tentativas == tentativas_maximas:
+    print("Número máximo de tentativas atingido. Acesso bloqueado.")
